594ebd267a39f20bbd2cd462574465ae915fb1f2
HansBlackCat <jeremy3599@snu.ac.kr>
2020-04-24 23:55:56 +0900

    2020-04-24 23:55:45

--- Hello.scala
+++ Hello.scala
@@ -1,4 +1,3 @@
-
 /*
 object Hello {
   def main(args: Array[String]) = {
@@ -7,4 +6,20 @@ object Hello {
 }
 */
 
-public 
+/*
+object Hello2 extends App {
+  println("Hello, world")
+}
+*/
+
+object HelloYou extends App {
+  if (args.size == 0) {
+    println("Hello, you")
+    println(args.size)
+  } else {
+    println("Hello, " + args(0))
+    println(args.size)
+  }
+}
+
+
