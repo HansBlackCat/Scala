30917f6fab2ede3e2d94f96551c084e945f59c92
HansBlackCat <jeremy3599@snu.ac.kr>
2020-05-08 23:01:29 +0900

    2020-05-08 23:01:18

--- Practice_Scala2/30_caseClass.scala
+++ Practice_Scala2/30_caseClass.scala
@@ -10,4 +10,13 @@ object main {
     val op = BinOp("+", Number(1), v)
     val v_name = v.name
     val op_copy = op.copy(operator = "-")
+
+    def simplifyTop(expr: Expr): Expr =
+        expr match {
+            case UnOp("-", UnOp("-",e))   => e
+            case Binop("+", e, Number(0)) => e 
+            case BinOp("*", e, Number(1)) => e
+            case _                        => expr
+        }
+    
 }
