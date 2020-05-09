c5026042133d1d4540102e325fda70a00beec231
HansBlackCat <jeremy3599@snu.ac.kr>
2020-05-09 19:48:06 +0900

    2020-05-09 19:47:53

--- Practice_Scala2/30_caseClass.scala
+++ Practice_Scala2/30_caseClass.scala
@@ -1,3 +1,4 @@
+import math._
 
 abstract class Expr
 case class Var(name: String) extends Expr
@@ -11,12 +12,49 @@ object main {
     val v_name = v.name
     val op_copy = op.copy(operator = "-")
 
-    def simplifyTop(expr: Expr): Expr =
+    def simplifyTop(expr: Expr): Expr = {
         expr match {
             case UnOp("-", UnOp("-",e))   => e
-            case Binop("+", e, Number(0)) => e 
+            case BinOp("+", e, Number(0)) => e 
             case BinOp("*", e, Number(1)) => e
             case _                        => expr
         }
-    
+    }
+
+    def binOpFind(expr: Expr) = {
+        expr match {
+            case BinOp(_,_,_) => println(s"$expr is a binary operation")
+            case _            => println("It's something else")
+        }
+    }
+
+    def describe(x: Any) = {
+        val pi = math.Pi
+        x match {
+            case 5 => "five"
+            case true => "truth"
+            case "hello" => "hi!"
+            // case Nil => "empty list"
+            case E => "It's exp"
+            case `pi` => "variable pi"
+            case List(0,_,_) => "List with first zero and 3 elements"
+            case List(1,_*) => "List with first one"
+            case List(_*) => "Universal list"
+            case _ => "something else"
+        }
+    }
+
+    def generalSize(x: Any) = {
+        x match {
+            case s: String => s.length
+            case m: Map[_,_] => m.size
+            case _ => -1
+        }
+    }
+
+    def isIntToIntMap(x: Any) =
+        x match {
+            case m: Map[Int, Int] => true
+            case _                => false
+        }
 }
