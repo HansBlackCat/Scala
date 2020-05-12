56de7bf357cc71c5d29657e69d3ba141f8d9d538
HansBlackCat <jeremy3599@snu.ac.kr>
2020-05-12 16:56:52 +0900

    2020-05-12 16:56:34

--- ML_withScala/src/main/scala/dev/mlScala/source/CSVfinance.scala
+++ ML_withScala/src/main/scala/dev/mlScala/source/CSVfinance.scala
@@ -1,5 +1,8 @@
 package dev.mlScala.source
 
+import scala.util.Try
+import scala.io.Source
+
 /*
  * type Features = Array[Double]
  * type Weight = Array[Double]
@@ -8,6 +11,7 @@ package dev.mlScala.source
  */
 
 object GoogleFinancials extends Enumeration {
+    import dev.mlScala.DataType._
     import dev.mlScala.FinanceType._
 
     type GoogleFinancials = Value
@@ -18,5 +22,18 @@ object GoogleFinancials extends Enumeration {
     }
     def toArray(vs: Array[Value]): Fields => Features = {
         (s: Fields) => vs.map(v => s(v.id).toDouble)
+    }
+    /*
+    def load(fileName: String): Try[Vector[DblPair]] = Try {
+        val src = Source.fromFile(fileName)
+        val data = extract(src.getLines.map(_.split(",")).drop(1))
+        src.close
+        data
+    } */   
+    def extract(cols: Iterator[Fields]): ObsSet = {
+        val features = Array[GoogleFinancials](LOW, HIGH, VOLUME)
+        val conversion = toArray(features)
+        cols.map(conversion(_)).toVector
+            .map(x => Array[Double](1.0 - x(0)/x(1), x(2)))
     }
 }
