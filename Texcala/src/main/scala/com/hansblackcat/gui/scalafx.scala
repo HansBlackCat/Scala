08896889ac3f9144198b4fb14a21dac75bc147dd
HansBlackCat <jeremy3599@snu.ac.kr>
2020-05-17 21:40:03 +0900

    2020-05-17 21:39:51

---
+++ Texcala/src/main/scala/com/hansblackcat/gui/scalafx.scala
@@ -0,0 +1,49 @@
+package com.hansblackcat.gui
+
+import scalafx.application.JFXApp
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color._
+import scalafx.scene.paint._
+import scalafx.scene.text.Text
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control.Alert
+
+object ScalaFXHelloWorld extends JFXApp {
+
+  stage = new PrimaryStage {
+    //    initStyle(StageStyle.Unified)
+    title = "Texrix"
+    scene = new Scene {
+      fill = Color.rgb(0, 0, 0)
+      content = new HBox {
+        padding = Insets(50, 80, 50, 80)
+        children = Seq(
+          new Text {
+            text = "Scala"
+            style = "-fx-font: normal bold 100pt sans-serif"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(Red, DarkRed))
+          },
+          new Text {
+            text = "FX"
+            style = "-fx-font: italic bold 100pt sans-serif"
+            fill = new LinearGradient(
+              endX = 0,
+              stops = Stops(White, DarkGray)
+            )
+            effect = new DropShadow {
+              color = DarkGray
+              radius = 15
+              spread = 0.25
+            }
+          }
+        )
+      }
+    }
+  }
+}
